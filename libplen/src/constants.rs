ab5caa42142f150bf0ef45356f8691b469294501
Malcolm Vigren <trekommafem2@gmail.com>
2021-01-31 09:42:06 +0100

    Fix menu controls but doesn't compile

--- libplen/src/constants.rs
+++ libplen/src/constants.rs
@@ -1,3 +1,6 @@
+pub const TEAM_RED_ID: u64 = 0;
+pub const TEAM_BLUE_ID: u64 = 1;
+
 // currently hardcoded to the background image size
 pub const WORLD_SIZE: f32 = 3000.;
 pub const DELTA_TIME: f32 = 0.01;
@@ -5,9 +8,8 @@ pub const SERVER_SLEEP_DURATION: u64 = 10;
 
 pub const WINDOW_SIZE: f32 = 700.;
 
-pub const MENU_BACKGROUND_COLOR: (u8, u8, u8) = (30, 30, 30);
-
-pub const NAME_POS: (f32, f32) = (50., 150.);
+pub const NAME_POS: (f32, f32) = (50., 100.);
+pub const STATUS_TEXT_POS: (f32, f32) = (50., 200.);
 
 pub const PLAYER_SPEED: f32 = 50.;
 
@@ -18,3 +20,16 @@ pub const DOORWAY_LENGTH: f32 = 0.75;
 pub const DOOR_WIDTH: f32 = 1.0;
 
 pub const SCREEN_PADDING: f32 = 0.5;
+
+pub const MENU_BACKGROUND_COLOR: (u8, u8, u8) = (30, 30, 30);
+pub const MENU_CENTER_LINE_COLOR: (u8, u8, u8) = (0, 0, 0);
+pub const MENU_CENTER_LINE_WIDTH: u32 = 10;
+pub const MENU_RED_BACKGROUND_COLOR: (u8, u8, u8) = (30, 0, 0);
+pub const MENU_BLUE_BACKGROUND_COLOR: (u8, u8, u8) = (0, 0, 30);
+
+pub const MENU_BUTTON_JOIN_DISPATCHER_Y: f32 = 1./3.;
+pub const MENU_BUTTON_JOIN_AGENT_Y: f32 = 1./2.;
+pub const MENU_BUTTON_HEIGHT: u32 = 30;
+pub const MENU_BUTTON_WIDTH: u32 = 100;
+pub const MENU_RED_BUTTON_COLOR: (u8, u8, u8) = (70, 10, 10);
+pub const MENU_BLUE_BUTTON_COLOR: (u8, u8, u8) = (10, 10, 70);
